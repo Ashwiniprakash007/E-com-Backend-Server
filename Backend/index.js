d977fd6cf9fc6c14b82a89f35898d388cf7b3b23
Ashwini Prakash <100183151+Ashwiniprakash007@users.noreply.github.com>
2024-04-14 02:07:59 +0530

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,29 @@ const express = require("express");
 const connection = require("./config/db")
 const cors = require('cors');
 const app = express();
-
+const corsOptions = {
+    origin: '*',
+  };
+  
+  app.use(cors(corsOptions));
+  app.use(express.json());
+  
+  // Increase server timeout
+  app.use((req, res, next) => {
+      res.setTimeout(120000, () => {
+        console.log('Request timed out');
+        res.status(500).json({ message: 'Request timed out' });
+      });
+      next();
+  });
+  
 const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
+
 
 
 app.get("/", (req, res) => {
